423d0a199a8a3ee598819c861eca1fa6b42a06d8
Lindsay <97534629+lindsayrg101@users.noreply.github.com>
2024-07-26 21:57:57 -0400

    Removed code

--- main.py
+++ main.py
@@ -14,22 +14,3 @@ def decode(password):
     return decoded
 
 
-while __name__ == '__main__':
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    print()
-    option = int(input('Please enter an option: '))
-    if option == 1:
-        password = input("Please enter the password to encode: ")
-        encoded_password = encode(password)
-        print('Your password has been encoded and stored!')
-        print()
-    elif option == 2:
-        decoded_password = decode(encoded_password)
-        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
-        print()
-    elif option == 3:
-        exit()
