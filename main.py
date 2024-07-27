65489e99f5865cca72110d54b438cf2864a81c45
Lindsay <97534629+lindsayrg101@users.noreply.github.com>
2024-07-26 20:27:51 -0400

    Commit
    changes

---
+++ main.py
@@ -0,0 +1,32 @@
+def encode(password):
+    encoded_password = ""
+    for char in password:
+        new_char = str((int(char) + 3) % 10)
+        encoded_password += new_char
+    return encoded_password
+
+def decode(password):
+    decoded_password = ""
+    for char in password:
+        new_char = str((int(char) - 3) % 10)
+        decoded_password += new_char
+    return decoded_password
+while __name__ == '__main__':
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+    option = int(input('Please enter an option: '))
+    if option == 1:
+        password = input("Please enter the password to encode: ")
+        encoded_password = encode(password)
+        print('Your password has been encoded and stored!')
+        print()
+    elif option == 2:
+        decoded_password = decode(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        print()
+    elif option == 3:
+        exit()
